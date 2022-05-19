c9d0075eea0958da1e2eea55b160e25d0f99b9ab
Daizi <860943443@qq.com>
2022-05-19 18:03:18 +0800

    优化

---
+++ src/main-prod.js
@@ -0,0 +1,50 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import './plugins/element.js'
+import './assets/css/global.css'
+
+// import Vue from 'vue'
+import ZkTable from 'vue-table-with-tree-grid'
+
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// 导入富文本编辑器对应样式
+// import 'quill/dist/quill.core.css' // import styles
+// import 'quill/dist/quill.snow.css' // for snow theme
+// import 'quill/dist/quill.bubble.css' // for bubble theme
+
+Vue.use(ZkTable)
+// Vue.component(ZkTable.name, ZkTable)
+
+Vue.use(VueQuillEditor)
+
+import axios from 'axios'
+import {Message} from "element-ui";
+axios.defaults.baseURL = 'https://lianghj.top:8888/api/private/v1/'
+Vue.prototype.axios = axios
+// axios 请求拦截器
+axios.interceptors.request.use(config => {
+  // console.log(config)
+  // 请求头设置
+  config.headers.Authorization = localStorage.getItem('token')
+  return config
+})
+// axios 响应拦截器
+axios.interceptors.response.use(response => {
+  // console.log(response)
+  if (response.data.meta.status != 200) Message.error(response.data.meta.msg)
+  return response
+},error => {
+  console.log(error)
+  return Promise.reject(error.response)  // 返回接口错误信息
+})
+
+Vue.config.productionTip = false
+
+
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,39 @@
 const { defineConfig } = require('@vue/cli-service')
+// const { config } = require('vue/types/umd')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+
+  // chainWebpack: config = () => {
+  //   config.when(process.env.NODE_ENV === 'production', config => {
+  //     config.entry('app').clear().add('./src/main-prod.js')
+  //   })
+
+  //   config.when(process.env.NODE_ENV === 'development', config => {
+  //     config.entry('app').clear().add('./src/main.js')
+  //   })
+  // }
+
+  // 发布模式
+  chainWebpack: config => {
+    config.when(process.env.NODE_ENV === 'production', config => {
+      config.entry('app').clear().add('./src/main-prod.js') 
+
+      config.set('externals', {
+        vue: 'Vue',
+        'vue-router': 'VueRouter',
+        axios: 'axios',
+        lodash: '_',
+        echarts: 'echarts',
+        nprogress: 'NProgress',
+        'vue-quill-editor': 'VueQuillEditor',
+        jquery: '$'
+      })
+    })
+
+    // 开发模式
+    config.when(process.env.NODE_ENV === 'development', config => {
+    config.entry('app').clear().add('./src/main.js')
+    })
+   }
 })
